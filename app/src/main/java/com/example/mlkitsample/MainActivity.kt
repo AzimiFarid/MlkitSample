7295a94b34610f76ea981e2ba67391f3d9d5c35c
F.Azimi <faridlotfabadi@gmail.com>
2022-07-20 15:01:33 +0430

    Some edits.

--- app/src/main/java/com/example/mlkitsample/MainActivity.kt
+++ app/src/main/java/com/example/mlkitsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mlkitsample
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -14,12 +15,12 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Size
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.core.view.doOnLayout
-import androidx.lifecycle.MutableLiveData
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
@@ -33,31 +34,26 @@ class MainActivity : AppCompatActivity() {
     lateinit var context: Context
     lateinit var messageTextView: TextView
     var left: Double = 0.0
-    var top: Double = 0.0
+    private var top: Double = 0.0
     private var right: Double = 0.0
     private var bottom: Double = 0.0
 
-    lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var cameraProvider: ProcessCameraProvider
 
     var savedUri = ""
     private lateinit var outputDirectory: File
     private val fileNameFormat = "yyyy-MM-dd-HH-mm-ss-SSS"
-
     private lateinit var containerFrameLayout: FrameLayout
     private lateinit var imageView: ImageView
     private lateinit var firstLinearLayout: LinearLayout
-    private lateinit var secondLinearLayout: LinearLayout
-    private lateinit var thirdLinearLayout: LinearLayout
+    var steps = 0
 
-    private val seriousUnicode = 0x1F610
-    private val blinkUnicode = 0x1F611
 
-    lateinit var selectedOperations :ArrayList<String>
+    private val LIVE_DETECTION_OPERATIONS = arrayListOf(
+        "blink", "smile", "turnToRight",
+        "turnToLeft", "turnToUp"
+    )
 
-    val stepCounter = MutableLiveData<Int>().apply { postValue(3) }
-
-    private val  LIVE_DETECTION_OPERATIONS = arrayListOf("blink", "smile", "turnToRight",
-        "turnToLeft", "turnToUp" )
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,22 +63,17 @@ class MainActivity : AppCompatActivity() {
         containerFrameLayout = findViewById(R.id.container_frame_layout)
         imageView = findViewById(R.id.image_view)
         firstLinearLayout = findViewById(R.id.first_layout)
-        secondLinearLayout = findViewById(R.id.second_layout)
-        thirdLinearLayout = findViewById(R.id.third_layout)
         messageTextView = findViewById(R.id.message_text_view)
 
         messageTextView.text =
-            resources.getString(R.string.take_camera_in_front_of_your_face) + "  " + getEmojiByUnicode(
-                seriousUnicode
-            )
-
+            resources.getString(R.string.take_camera_in_front_of_your_face)
         drawOval()
         startCamera()
 
     }
 
     private fun drawOval() {
-        containerFrameLayout.doOnLayout { it ->
+        containerFrameLayout.doOnLayout {
             val bitmap: Bitmap = Bitmap.createBitmap(
                 it.measuredWidth,
                 it.measuredHeight,
@@ -106,7 +97,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError", "SetTextI18n",
+    @SuppressLint(
+        "UnsafeExperimentalUsageError", "UnsafeOptInUsageError", "SetTextI18n",
         "RestrictedApi"
     )
     private fun startCamera() {
@@ -156,69 +148,74 @@ class MainActivity : AppCompatActivity() {
         )
 
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        val selectedOperations = getTripleOperations() as ArrayList<String>
+
 
         imageAnalysis.setAnalyzer(
             ContextCompat.getMainExecutor(this)
         ) { imageProxy ->
             val mediaImage = imageProxy.image
             if (mediaImage != null) {
-
                 val image =
                     InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
                 detector.process(image)
                     .addOnSuccessListener { faces ->
                         for (face in faces) {
-                            if (face.boundingBox.left < 0 || face.boundingBox.top < 0) {
-                                messageTextView.text =
-                                    resources.getString(R.string.put_your_face_in_the_frame)
-                            } else {
-                                imageCapture.takePicture(
-                                    outputOptions,
-                                    ContextCompat.getMainExecutor(this),
-                                    object : ImageCapture.OnImageSavedCallback {
-                                        override fun onError(exc: ImageCaptureException) {
-                                            Log.e(
-                                                "",
-                                                "Photo capture failed: ${exc.message}",
-                                                exc
+                            steps++
+                            if (steps < 3) {
+                                if (face.boundingBox.left < 0 || face.boundingBox.top < 0) {
+                                    messageTextView.text =
+                                        resources.getString(R.string.put_your_face_in_the_frame)
+                                } else {
+                                    imageCapture.takePicture(
+                                        outputOptions,
+                                        ContextCompat.getMainExecutor(this),
+                                        object : ImageCapture.OnImageSavedCallback {
+                                            override fun onError(exc: ImageCaptureException) {
+                                                Log.e(
+                                                    "",
+                                                    "Photo capture failed: ${exc.message}",
+                                                    exc
+                                                )
+                                            }
+
+                                            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                                                savedUri =
+                                                    Uri.fromFile(photoFile).toString()
+                                            }
+                                        })
+                                    messageTextView.text = selectedOperations[0]
+
+                                    //TODO : choose random operations
+                                    when (steps) {
+
+                                        1 -> {
+                                            chooseFunctionFromFunctionName(
+                                                selectedOperations[0],
+                                                face
                                             )
+                                            messageTextView.text = selectedOperations[1]
+
+                                        }
+                                        2 -> {
+                                            chooseFunctionFromFunctionName(
+                                                selectedOperations[1],
+                                                face
+                                            )
+                                            messageTextView.text = selectedOperations[2]
+
+                                        }
+                                        3 -> {
+                                            chooseFunctionFromFunctionName(
+                                                selectedOperations[2],
+                                                face
+                                            )
+                                            messageTextView.text = "Congrats! It is OK."
+
                                         }
 
-                                        override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                                            savedUri =
-                                                Uri.fromFile(photoFile).toString()
-                                        }
-                                    })
-                                firstLinearLayout.setBackgroundColor(
-                                    ContextCompat.getColor(
-                                        this,
-                                        R.color.purple_200
-                                    )
-                                )
-                                //TODO : choose random operations
-                                selectedOperations = getTripleOperations() as ArrayList<String>
-                                stepCounter.observe(this) {
-                                    when (stepCounter.value) {
-                                        1 -> chooseFunctionFromFunctionName(
-                                            selectedOperations[0],
-                                            face
-                                        )
-                                        2 -> chooseFunctionFromFunctionName(
-                                            selectedOperations[1],
-                                            face
-                                        )
-                                        3 -> chooseFunctionFromFunctionName(
-                                            selectedOperations[2],
-                                            face
-                                        )
-                                    }
-                                    if (stepCounter.value == 3) {
-                                        Toast.makeText(this, "It is DONE!", Toast.LENGTH_LONG)
-                                            .show()
                                     }
                                 }
-
-
                             }
 
                             if (face.trackingId != null) {
@@ -252,11 +249,6 @@ class MainActivity : AppCompatActivity() {
             mediaDir else filesDir
     }
 
-    fun getEmojiByUnicode(unicode: Int): String {
-        return String(Character.toChars(unicode))
-    }
-
-
     @SuppressLint("SetTextI18n")
     private fun blink(face: Face) {
         if (face.rightEyeOpenProbability != null && face.rightEyeOpenProbability != null) {
@@ -264,106 +256,81 @@ class MainActivity : AppCompatActivity() {
             val leftEyeOpenProb = face.leftEyeOpenProbability
 
             if (rightEyeOpenProb!! < 0.1 && leftEyeOpenProb!! < 0.1) {
-                secondLinearLayout.setBackgroundColor(
-                    ContextCompat.getColor(
-                        this,
-                        R.color.purple_200
-                    )
-                )
-                messageTextView.text =
-                    resources.getString(R.string.please_blink) + "   " + getEmojiByUnicode(
-                        blinkUnicode
-                    )
-
-
+                Log.d("OPERATOR >>> ", "blink")
+                Log.d("STEP >>> ",steps.toString())
+                steps++
             }
         }
     }
 
+
+    @SuppressLint("SetTextI18n")
     private fun smile(face: Face) {
         if (face.smilingProbability != null) {
             if (face.smilingProbability!! > 0.9) {
-                thirdLinearLayout.setBackgroundColor(
-                    ContextCompat.getColor(
-                        this,
-                        R.color.purple_200
-                    )
-                )
-                Toast.makeText(this , "It is Ok!" , Toast.LENGTH_LONG).show()
+                Log.d("OPERATOR >>> ", "smile")
+                Log.d("STEP >>> ",steps.toString())
+                steps++
             }
         }
+
     }
 
+
+    @SuppressLint("SetTextI18n")
     private fun turnToRight(face: Face) {
 
         if (face.headEulerAngleZ > 40) {
-            Log.d("headEulerAngleZ >>> ", face.headEulerAngleZ.toString())
-            thirdLinearLayout.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.purple_200
-                )
-            )
-            Toast.makeText(this, "It is OK!", Toast.LENGTH_LONG).show()
-
-
+            Log.d("OPERATOR >>> ", "turnToRight")
+            Log.d("STEP >>> ",steps.toString())
+            steps++
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun turnToLeft(face: Face) {
 
         if (face.headEulerAngleY > 40) {
-            Log.d("headEulerAngleY >>> ", face.headEulerAngleY.toString())
-            thirdLinearLayout.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.purple_200
-                )
-            )
-            Toast.makeText(this, "It is OK!", Toast.LENGTH_LONG).show()
-
+            Log.d("OPERATOR >>> ","turnToLeft")
+            Log.d("STEP >>> ",steps.toString())
+            steps++
 
         }
+
     }
 
+    @SuppressLint("SetTextI18n")
     private fun turnToUp(face: Face) {
 
         if (face.headEulerAngleX > 40) {
-            Log.d("headEulerAngleX >>> ", face.headEulerAngleX.toString())
-            thirdLinearLayout.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.purple_200
-                )
-            )
-            Toast.makeText(this, "It is OK!", Toast.LENGTH_LONG).show()
-
+            Log.d("OPERATOR >>> ", "turnToUp")
+            Log.d("STEP >>> ",steps.toString())
+            steps++
 
         }
     }
 
 
-    private fun chooseFunctionFromFunctionName(functionName : String , face: Face){
-        stepCounter.value?.minus(1)
-        when(functionName){
+    private fun chooseFunctionFromFunctionName(functionName: String, face: Face) {
+        when (functionName) {
             LIVE_DETECTION_OPERATIONS[0] -> blink(face)
             LIVE_DETECTION_OPERATIONS[1] -> smile(face)
             LIVE_DETECTION_OPERATIONS[2] -> turnToRight(face)
             LIVE_DETECTION_OPERATIONS[3] -> turnToLeft(face)
             LIVE_DETECTION_OPERATIONS[4] -> turnToUp(face)
         }
+
     }
 
-    private fun getTripleOperations() : MutableList<String> {
-        val list : MutableList<String> = mutableListOf()
-        var tempList = LIVE_DETECTION_OPERATIONS.toMutableList()
-        for (i in 0..2){
+    private fun getTripleOperations(): MutableList<String> {
+        val list: MutableList<String> = mutableListOf()
+        val tempList = LIVE_DETECTION_OPERATIONS.toMutableList()
+        for (i in 0..2) {
             val selectedItem = tempList.random()
             list.add(selectedItem)
             tempList.remove(selectedItem)
         }
         return list
     }
-
 
 }
